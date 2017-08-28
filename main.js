c32f43228de90ab08d4fdc088002ba522df04071
Ronnie Hickman <rhickman2305@gmail.com>
2017-08-28 18:16:53 -0500

    json ex

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
 
-var catDiv = document.getElementById('kat-koral');
-function domString(event){
+var animalDiv = document.getElementById('kat-koral');
+function domString(data){
 	var catString = "";
-	for(var i = 0; i < event.length; i++){
+	for(var i = 0; i < data.length; i++){
 		catString += `<div class="cat">`
-		catString += `<div>${event[i].name}</div>`
-		catString += `<img class="catImage" src="${event[i].url}">`
+		catString += `<div>${data[i].name}</div>`
+		catString += `<img class="catImage" src="${data[i].url}">`
 		catString += `</div>`
-}
-writeToDom(catString);
-
-function writeToDom(strang){
-	catDiv.innerHTML = strang;
-
 	}
+	writeToDom(catString);
+}
+function writeToDom(strang){
+	animalDiv.innerHTML += strang;
 
 }
 
-domString(cats);
+function executeThisCodeAfterFileLoads(){
+	console.log("this", this.responseText);
+	var data = JSON.parse(this.responseText);
+	domString(data.cats);
+}
+
+function executeThisCodeIfFileErrors(){
+	console.log("Shit Broke");
+}
+
+function executeThisCodeAfterFileLoads2(){
+	console.log("this", this.responseText);
+	var data = JSON.parse(this.responseText);
+	domString(data.dogs);
+}
+
+function executeThisCodeIfFileErrors2(){
+	console.log("Shit Broke");
+}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
+myRequest.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest.open("GET", "cats.json");
+myRequest.send();
+
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", executeThisCodeAfterFileLoads2);
+myRequest2.addEventListener("error", executeThisCodeIfFileErrors2);
+myRequest2.open("GET", "dogs.json");
+myRequest2.send();
